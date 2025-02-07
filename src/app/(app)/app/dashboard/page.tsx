3a0941c4ff705e691f1ff8cd1422b0173d476f45
Luan Vendt <luan.vendt@bitize.com.br>
2025-02-07 11:32:31 -0300

    feat: add pet management components to dashboard; implement search form and content blocks

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,4 +1,8 @@
 import Branding from "@/components/branding";
+import ContentBlock from "@/components/content-block";
+import PetDetails from "@/components/pet-details";
+import PetList from "@/components/pet-list";
+import SearchForm from "@/components/search-form";
 import Stats from "@/components/stats";
 
 export default function Dashboard() {
@@ -8,6 +12,24 @@ export default function Dashboard() {
         <Branding />
 
         <Stats />
+      </div>
+
+      <div className="grid md:grid-cols-3 md:grid-rows-[45px_1fr] grid-rows-[45px_300px_500px] gap-4 md:h-[600px]">
+        <div className="md:row-start-1 md:row-span-1 md:ol-start-1 md:col-span-1">
+          <SearchForm />
+        </div>
+
+        <div className="md:row-start-2 md:row-span-full md:col-start-1 md:col-span-1">
+          <ContentBlock>
+            <PetList />
+          </ContentBlock>
+        </div>
+
+        <div className="md:row-star-1 md:row-span-full md:col-start-2 md:col-span-full">
+          <ContentBlock>
+            <PetDetails />
+          </ContentBlock>
+        </div>
       </div>
     </main>
   );
