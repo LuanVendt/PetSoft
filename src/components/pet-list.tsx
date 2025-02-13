e1dc51649d703e72ea8491c40fb2a44eccff6c2d
Luan Vendt <luan.vendt@bitize.com.br>
2025-02-13 10:38:16 -0300

    feat: implement PetContextProvider for pet data management; refactor Dashboard and PetList components to use context

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,7 +1,11 @@
-import { PetListProps } from "@/lib/types";
+"use client";
+
+import { usePetContext } from "@/lib/hooks";
 import Image from "next/image";
 
-export default function PetList({ pets }: PetListProps) {
+export default function PetList() {
+  const { pets } = usePetContext();
+
   return (
     <ul className="bg-white border-b border-black/[0.08]">
       {pets.map((pet) => (
