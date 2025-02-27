6376c04c6808f9227c70751ce3ca797d249213e4
Luan Vendt <luan.vendt@bitize.com.br>
2025-02-27 15:05:13 -0300

    feat: integrate react-hook-form and zod for form validation in PetForm component

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+import { DEFAULT_PET_IMAGE } from "@/lib/constants";
 import { usePetContext } from "@/lib/hooks";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import PetFormBtn from "./pet-form-btn";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
@@ -11,15 +15,59 @@ type PetFormProps = {
   onFormSubmission: () => void;
 };
 
+type TPerForm = {
+  name: string;
+  ownerName: string;
+  imageUrl: string;
+  age: number;
+  notes: string;
+};
+
+const petFormSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(1, { message: "Name is required" })
+    .max(7, { message: "Name should be less than 7 characters" }),
+  ownerName: z
+    .string()
+    .trim()
+    .min(1, { message: "Owner Name is required" })
+    .max(20, { message: "Owner Name should be less than 20 characters" }),
+  imageUrl: z.union([
+    z.literal(""),
+    z.string().url({ message: "Invalid URL" }),
+  ]),
+  age: z.coerce.number().int().max(999).positive(),
+  notes: z.union([
+    z.literal(""),
+    z
+      .string()
+      .trim()
+      .max(100, { message: "Notes should be less than 100 characters" }),
+  ]),
+});
+
 export default function PetForm({
   actionType,
   onFormSubmission,
 }: PetFormProps) {
   const { selectedPet, handleAddPet, handleEditPet } = usePetContext();
 
+  const {
+    register,
+    formState: { errors },
+    trigger,
+  } = useForm<TPerForm>({
+    resolver: zodResolver(petFormSchema),
+  });
+
   return (
     <form
       action={async (formData) => {
+        const result = await trigger();
+        if (!result) return;
+
         onFormSubmission();
 
         const petData = {
@@ -41,63 +89,68 @@ export default function PetForm({
           <Label htmlFor="name">Name</Label>
           <Input
             id="name"
-            type="text"
-            name="name"
-            maxLength={10}
-            required
+            {...register("name")}
+            maxLength={7}
             defaultValue={actionType === "edit" ? selectedPet?.name : ""}
             placeholder="Name"
           />
+          {errors.name && <p className="text-red-500">{errors.name.message}</p>}
         </div>
 
         <div>
           <Label htmlFor="ownerName">Owner Name</Label>
           <Input
             id="ownerName"
-            type="text"
-            name="ownerName"
-            maxLength={20}
-            required
+            {...register("ownerName")}
             defaultValue={actionType === "edit" ? selectedPet?.ownerName : ""}
             placeholder="Owner Name"
           />
+          {errors.ownerName && (
+            <p className="text-red-500">{errors.ownerName.message}</p>
+          )}
         </div>
 
         <div>
           <Label htmlFor="imageUrl">Image Url</Label>
           <Input
             id="imageUrl"
-            type="text"
-            name="imageUrl"
-            maxLength={200}
-            defaultValue={actionType === "edit" ? selectedPet?.imageUrl : ""}
+            {...register("imageUrl")}
+            defaultValue={
+              actionType === "edit" &&
+              selectedPet?.imageUrl !== DEFAULT_PET_IMAGE
+                ? selectedPet?.imageUrl
+                : ""
+            }
             placeholder="Unsplash image URL"
           />
+          {errors.imageUrl && (
+            <p className="text-red-500">{errors.imageUrl.message}</p>
+          )}
         </div>
 
         <div>
           <Label htmlFor="age">Age</Label>
           <Input
             id="age"
-            type="number"
-            name="age"
-            min={0}
-            required
+            {...register("age")}
             defaultValue={actionType === "edit" ? selectedPet?.age : ""}
             placeholder="Age"
           />
+          {errors.age && <p className="text-red-500">{errors.age.message}</p>}
         </div>
 
         <div>
           <Label htmlFor="notes">Notes</Label>
           <Textarea
             id="notes"
-            rows={3}
-            name="notes"
-            maxLength={150}
+            {...register("notes")}
+            maxLength={100}
             defaultValue={actionType === "edit" ? selectedPet?.notes : ""}
             placeholder="Notes"
           />
+          {errors.notes && (
+            <p className="text-red-500">{errors.notes.message}</p>
+          )}
         </div>
       </div>
 
